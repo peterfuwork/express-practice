c88f96b6343a084e83c001be691a121bb3b9ace3
Chiaen Fu <peterfuwork@gmail.com>
2018-11-19 11:51:08 -0700

    updated some code

--- index.js
+++ index.js
@@ -4,14 +4,15 @@ const http = require('http');
 const cors = require('cors');
 const morgan = require('morgan');
 const app = express();
-const router = require('./router');
-
-router(app);
 
 app.disable('x-powered-by');
 app.use(morgan('combined'));
 app.use(cors());
 app.use(bodyParser.json({ type: '*/*' }));
+
+// router need to be underneath body-parser line
+const router = require('./router');
+router(app);
 
 const PORT = process.env.PORT || 8000;
 const server = http.createServer(app);
--- router.js
+++ router.js
@@ -1,7 +1,59 @@
 module.exports = function(app) {
-    app.get('/:name', function(req, res) {
-        res.send("Your Name: " + req.params.name );
+    let cars = require('./data');
+
+    // get method
+    app.get('/cars', function(req, res) {
+        res.send({
+            cars
+        });
     });
+
+    // post method
+    app.post('/cars', (req, res, next) => {
+        const body = req.body;
+        const newObj = {
+            id: cars.length + 1,
+            brand: body.brand,
+            model: body.model
+        };
+        cars.push(newObj);
+        res.send({ cars: newObj })
+    });
+
+    // put method
+    app.put('/cars/:id', (req, res, next) => {
+        const id = Number.parseInt(req.params.id);
+        const body = req.body;
+        const newArr = cars.map(obj => {
+            if(obj.id === id) {
+                obj.brand = body.brand;
+            };
+            return obj;
+        });
+        res.send({ cars: newArr })
+    })
+
+
+
+    // get by id
+    app.get('/cars/:id', function(req, res) {
+        const id = Number.parseInt(req.params.id);
+        const matching = cars.filter(item => item.id === id)
+        res.send({ 
+            cars: matching
+        });
+    });
+
+    
+    app.get('/cars/:brand', function(req, res) {
+        const brand = req.params.brand;
+        const matching = cars.filter(item => item.brand.toLowerCase().trim() === brand.toLowerCase().trim())
+        console.log(matching);
+        res.send({
+            cars: matching
+        });
+    });
+
 
     app.use((err, req, res, next) => {
         const status = err.status || 500;
